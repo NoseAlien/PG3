7ee735bfbdfb64000a36033f5d2b199184f6262e
NoseAlien <k021g1117@g.neec.ac.jp>
2022-10-31 10:15:08 +0900

    2-2

--- main.cpp
+++ main.cpp
@@ -1,58 +1,87 @@
 #include <stdio.h>
-#include <list>
-#include <vector>
+#include <stdlib.h>
+#include <string.h>
+
+struct rentalInfo
+{
+	char name[64];
+	int rentDate;
+	int returnDate;
+};
+
+typedef struct cell
+{
+	rentalInfo val;
+	struct cell* prev;
+	struct cell* next;
+}CELL;
+
+void create(CELL* begin, rentalInfo val);
+void index(CELL* begin);
+void indexBack(CELL* begin);
 
 int main()
 {
-	std::list<const char*> yamanoteLine
-	{
-		"Tokyo","Kanda","Akihabara","Okachimachi",
-		"Ueno","Uguisudani","Nippori","Tabata","Komagome",
-		"Sugamo","O-tsuka","Ikebukuro","Mejiro","Takadanobaba",
-		"Shin_O-kubo","Shinjuku","Yoyogi","Harajuku",
-		"Shibuya","Ebisu","Meguro","Gotanda","O-saki",
-		"Shinagawa","Tamachi","Hamamatsucho-","Shinbashi",
-		"Yurakucho-"
-	};
+	CELL head;
+	head.prev = nullptr;
+	head.next = nullptr;
 
-	printf("1970\n");
-	for (const auto& station : yamanoteLine)
-	{
-		printf("%s, ", station);
-	}
-	printf("\n\n");
+	create(&head, { "ìmñÿ",20220222,20220223 });
+	create(&head, { "ÉÅÉOÉä",20220406,20220407 });
+	create(&head, { "Ç’ÇËÉOÉä",20220807,20220808 });
+	create(&head, { "Ç»Ç’ÇË",20220906,20220907 });
 
-	for (std::list<const char*>::iterator itr = yamanoteLine.begin(); itr != yamanoteLine.end(); ++itr)
-	{
-		if (*itr == "Tabata")
-		{
-			itr = yamanoteLine.insert(itr, "Nishi_Nippori");
-			itr++;
-		}
-	}
+	index(&head);
 
-	printf("2019\n");
-	for (const auto& station : yamanoteLine)
-	{
-		printf("%s, ", station);
-	}
-	printf("\n\n");
-
-	for (std::list<const char*>::iterator itr = yamanoteLine.begin(); itr != yamanoteLine.end(); ++itr)
-	{
-		if (*itr == "Tamachi")
-		{
-			itr = yamanoteLine.insert(itr, "Takanawa_Gateway");
-			itr++;
-		}
-	}
-
-	printf("2022\n");
-	for (const auto& station : yamanoteLine)
-	{
-		printf("%s, ", station);
-	}
-	printf("\n\n");
+	indexBack(&head);
 
 	return 0;
+}
+
+void create(CELL* begin, rentalInfo val)
+{
+	CELL* newCell;
+	newCell = (CELL*)malloc(sizeof(CELL));
+
+	strcpy_s(newCell->val.name, 64, val.name);
+	newCell->val = val;
+	newCell->prev = nullptr;
+	newCell->next = nullptr;
+
+	CELL* targetCell = begin;
+
+	while (targetCell->next != nullptr)
+	{
+		targetCell = targetCell->next;
+	}
+
+	targetCell->next = newCell;
+	newCell->prev = targetCell;
+}
+
+void index(CELL* begin)
+{
+	CELL* targetCell = begin;
+
+	while (targetCell->next != nullptr)
+	{
+		targetCell = targetCell->next;
+		printf("%s\n%d\n%d\n\n",targetCell->val.name, targetCell->val.rentDate, targetCell->val.returnDate);
+	}
+}
+
+void indexBack(CELL* begin)
+{
+	CELL* targetCell = begin;
+
+	while (targetCell->next != nullptr)
+	{
+		targetCell = targetCell->next;
+	}
+
+	while (targetCell->prev != nullptr)
+	{
+		printf("%s\n%d\n%d\n\n", targetCell->val.name, targetCell->val.rentDate, targetCell->val.returnDate);
+		targetCell = targetCell->prev;
+	}
 }
